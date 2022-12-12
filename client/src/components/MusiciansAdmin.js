b4ec877ae4fff13626c4a3c6bfba348617c3a932
Mahipal Singh <mahipal@github.com>
2022-12-12 16:39:24 +0530

    Added change

--- client/src/components/MusiciansAdmin.js
+++ client/src/components/MusiciansAdmin.js
@@ -1,87 +1,115 @@
-import React, { Component, Fragment } from 'react';
-import Musician from './Musician';
+import React, { Component, Fragment } from "react";
+import Musician from "./Musician";
 import axios from "axios";
 
 export default class MusiciansAdmin extends Component {
-
   state = {
-    newmusician: { 
+    newmusician: {
       firstName: "",
       lastName: "",
-      genre: ""
+      genre: "",
     },
-    musicians: []
-  }
+    musicians: [],
+  };
 
   outputHTML = "";
 
-  handleAddMusician = async(id, event) => {
+  handleAddMusician = async (id, event) => {
     event.preventDefault();
     try {
       await axios.put(`/musician/${id}`, this.state.newmusician);
-      this.setState({ musicians: [...this.state.musicians, this.state.newmusician] });
-      this.setState({ newmusician: { "firstName": "", "lastName": "", "genre": "" }});
-    }catch(err) {
+      this.setState({
+        musicians: [...this.state.musicians, this.state.newmusician],
+      });
+      this.setState({
+        newmusician: { firstName: "", lastName: "", genre: "" },
+      });
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   handleUpdateMusician = (id, name) => {
     // add call to AWS API Gateway update musician endpoint here
-    const musicianToUpdate = [...this.state.musicians].find(musician => musician.id === id);
-    const updatedMusicians = [...this.state.musicians].filter(musician => musician.id !== id);
+    const musicianToUpdate = [...this.state.musicians].find(
+      (musician) => musician.id === id
+    );
+    const updatedMusicians = [...this.state.musicians].filter(
+      (musician) => musician.id !== id
+    );
     musicianToUpdate.musicianname = name;
     updatedMusicians.push(musicianToUpdate);
-    this.setState({musicians: updatedMusicians});
-  }
+    this.setState({ musicians: updatedMusicians });
+  };
 
-  handleDeleteMusician = async(id, event) => {
+  handleDeleteMusician = async (id, event) => {
     // using firstName as the id in musicians state
     event.preventDefault();
     try {
       await axios.delete(`/musician/${id}`);
-      const updatedMusicians = await [...this.state.musicians].filter(musician => musician.firstName.toLowerCase() !== id);
-      this.setState({musicians: updatedMusicians});
-    }catch(err) {
+      const updatedMusicians = await [...this.state.musicians].filter(
+        (musician) => musician.firstName.toLowerCase() !== id
+      );
+      this.setState({ musicians: updatedMusicians });
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
-  fetchMusicians = async() => {
-    try{
-      const res = await axios.get('/musician/all');
+  fetchMusicians = async () => {
+    try {
+      const res = await axios.get("/musician/all");
       const allMusicians = Object.keys(res.data).map((key) => {
         return res.data[key];
       });
       this.setState({ musicians: [...allMusicians] });
-    }catch(err) {
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
-  onAddMusicianFirstNameChange = event => this.setState({ newmusician: { ...this.state.newmusician, "firstName": event.target.value } });
-  onAddMusicianLastNameChange = event => this.setState({ newmusician: { ...this.state.newmusician, "lastName": event.target.value } });
-  onAddMusicianGenreChange = event => this.setState({ newmusician: { ...this.state.newmusician, "genre": event.target.value } });
+  onAddMusicianFirstNameChange = (event) =>
+    this.setState({
+      newmusician: { ...this.state.newmusician, firstName: event.target.value },
+    });
+  onAddMusicianLastNameChange = (event) =>
+    this.setState({
+      newmusician: { ...this.state.newmusician, lastName: event.target.value },
+    });
+  onAddMusicianGenreChange = (event) =>
+    this.setState({
+      newmusician: { ...this.state.newmusician, genre: event.target.value },
+    });
 
   componentDidMount = () => {
     this.fetchMusicians();
-  }
+  };
 
   render() {
     return (
       <Fragment>
         <section className="section">
           <div className="container">
-            <p className="subtitle is-5">Add and remove musicians using the form below:</p>
+            <p className="subtitle is-5">
+              Add and remove musicians using the form below this is from the
+              updated version:
+            </p>
             <br />
             <div className="columns">
               <div className="column is-one-third">
-                <form onSubmit={event => this.handleAddMusician(this.state.newmusician.firstName.toLowerCase(), event)}>
+                <form
+                  onSubmit={(event) =>
+                    this.handleAddMusician(
+                      this.state.newmusician.firstName.toLowerCase(),
+                      event
+                    )
+                  }
+                >
                   <div className="field">
                     <div className="control">
-                      <input 
+                      <input
                         className="input is-medium"
-                        type="text" 
+                        type="text"
                         placeholder="Enter first name"
                         value={this.state.newmusician.firstName}
                         onChange={this.onAddMusicianFirstNameChange}
@@ -90,9 +118,9 @@ export default class MusiciansAdmin extends Component {
                   </div>
                   <div className="field">
                     <div className="control">
-                      <input 
+                      <input
                         className="input is-medium"
-                        type="text" 
+                        type="text"
                         placeholder="Enter last name"
                         value={this.state.newmusician.lastName}
                         onChange={this.onAddMusicianLastNameChange}
@@ -101,9 +129,9 @@ export default class MusiciansAdmin extends Component {
                   </div>
                   <div className="field">
                     <div className="control">
-                      <input 
+                      <input
                         className="input is-medium"
-                        type="text" 
+                        type="text"
                         placeholder="Enter genre"
                         value={this.state.newmusician.genre}
                         onChange={this.onAddMusicianGenreChange}
@@ -112,7 +140,10 @@ export default class MusiciansAdmin extends Component {
                   </div>
                   <div className="field">
                     <div className="control">
-                      <button type="submit" className="button is-primary is-medium">
+                      <button
+                        type="submit"
+                        className="button is-primary is-medium"
+                      >
                         Add musician
                       </button>
                     </div>
@@ -120,25 +151,24 @@ export default class MusiciansAdmin extends Component {
                 </form>
               </div>
               <div className="column is-two-thirds">
-                {
-                  this.state.musicians.map((musician, index) =>
-                    <Musician
-                        isAdmin={true}
-                        handleUpdateMusician={this.handleUpdateMusician}
-                        handleDeleteMusician={this.handleDeleteMusician}
-                        firstname={musician.firstName}
-                        lastname={musician.lastName}
-                        genre={musician.genre}
-                        key={musician.lastName.toLowerCase()}
-                        id={musician.firstName.toLowerCase()}
-                        index={index}
-                      />
-                  )}
+                {this.state.musicians.map((musician, index) => (
+                  <Musician
+                    isAdmin={true}
+                    handleUpdateMusician={this.handleUpdateMusician}
+                    handleDeleteMusician={this.handleDeleteMusician}
+                    firstname={musician.firstName}
+                    lastname={musician.lastName}
+                    genre={musician.genre}
+                    key={musician.lastName.toLowerCase()}
+                    id={musician.firstName.toLowerCase()}
+                    index={index}
+                  />
+                ))}
               </div>
             </div>
           </div>
         </section>
       </Fragment>
-    )
+    );
   }
 }
